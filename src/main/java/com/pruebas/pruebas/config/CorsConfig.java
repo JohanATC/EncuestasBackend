96a1d4bc52f71a911f74328f8f2c7ec4e5288852
JohanATC <johanteran.1750@gmail.com>
2025-10-23 12:31:06 -0500

    importaciones pa el cors

--- src/main/java/com/pruebas/pruebas/config/CorsConfig.java
+++ src/main/java/com/pruebas/pruebas/config/CorsConfig.java
@@ -3,6 +3,11 @@ package com.pruebas.pruebas.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
